effeef573c40a82be9b6c62703a624ec70977c7c
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-04-14 03:10:52 +0200

    Add responsive styling

--- src/components/Reviews/Card/Card.jsx
+++ src/components/Reviews/Card/Card.jsx
@@ -1,59 +1,73 @@
 import React from "react";
 import styled from "styled-components/macro";
-import {FaQuoteLeft} from 'react-icons/fa';
-import {Avatar} from '@material-ui/core';
+import { FaQuoteLeft } from "react-icons/fa";
+import { Avatar } from "@material-ui/core";
 
 const CarouselContainer = styled.div`
   width: 600px;
   padding-top: 1rem;
   margin-top: 2rem;
   border-radius: 0 0 5px 5px;
-  box-shadow: 4px 4px 29px rgba(145, 142, 142, 0.25);
+  box-shadow: ${({ theme }) => theme.shadows.shadow1};
   transform: rotate(2deg);
-  ${({reverse}) => reverse && "transform: rotate(-2deg)"}
-  `;
+
+  @media screen and (max-width: 1050px) {
+    width: 500px;
+  }
+  @media screen and (max-width: 961px) {
+    width: 400px;
+  }
+  @media (max-width: 420px) {
+    width: 280px;
+  }
+  ${({ reverse }) => reverse && "transform: rotate(-2deg)"}
+`;
 
 const CardDetails = styled.div`
   text-align: left;
   padding: 1rem 2rem 0.5rem;
   font-wight: 700;
   font-size: 18px;
-  line-height: 22px
-`
-const CardHeading = styled.div`
+  line-height: 22px;
+`;
+
+const CardFooter = styled.div`
   display: flex;
   align-items: center;
-  padding-left: 1rem;
-  background-image: linear-gradient(91.29deg, #2D50EF 0%, #4ADEDE 100%);
-  border-radius: 0 0 4px 4px;
+  padding: 0.5rem 1rem;
+  background: ${({ theme }) => theme.gradients.gradient5};
+  border-radius: 0 0 5px 5px;
   text-align: left;
-  margin: 0;
+  margin-top: 1rem;
   color: white;
 `;
 
 const AvatarWrapper = styled.figure`
-  margin:0 1rem 0 0;
+  margin: 0 1rem 0 0;
   padding: 0;
-`
+`;
 
 const Card = ({ title, revRotate }) => {
   return (
     <div style={{ padding: "1rem" }}>
       <CarouselContainer reverse={revRotate}>
         <CardDetails>
-          <FaQuoteLeft size={35} style={{color: "#ddd"}}/>
+          <FaQuoteLeft
+            size={35}
+            style={{ color: "#ddd", marginBottom: "1rem" }}
+          />
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus
-            mauris ac convallis lobortis. Nulla facilisi. Cras egestas
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            dapibus mauris ac convallis lobortis. Nulla facilisi. Cras egestas
             sollicitudin magna, sed feugiat nibh. Aenean gravida
           </p>
         </CardDetails>
-        <CardHeading>
+        <CardFooter>
           <AvatarWrapper>
             <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
           </AvatarWrapper>
           <h2>{title}</h2>
-        </CardHeading>
+        </CardFooter>
       </CarouselContainer>
     </div>
   );
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,13 +1,27 @@
 import OwlCarousel from "react-owl-carousel";
-import Card from './Card/Card'
+import Card from "./Card/Card";
 import "owl.carousel/dist/assets/owl.carousel.min.css";
 import "owl.carousel/dist/assets/owl.theme.default.min.css";
+import styled from "styled-components/macro";
 
+const SectionHeading = styled.h2`
+  font-size: 46px;
+  line-height: 56px;
+  letter-spacing: 0em;
+  text-align: center;
+  background-size: 100%;
+  // ** Making a gradient text **
+  background-image: ${({ theme }) => theme.gradients.gradient5};
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-text-fill-color: transparent;
+`;
 
 const Reviews = () => {
   return (
     <>
-      <h2>What People Say</h2>
+      <SectionHeading>What People Say</SectionHeading>
 
       <OwlCarousel
         items="2"
@@ -16,15 +30,23 @@ const Reviews = () => {
         autoplay
         dots={false}
         loop
-        slideSpeed = "220"
         autoplayHoverPause
         mouseDrag
         touchDrag
         className="owl-theme"
+        responsive={{
+          0: { items: 1, margin: 150 },
+          480: { items: 1, margin: 150 },
+          550: { items: 2, margin: 300 },
+          678: { items: 2, margin: 150 },
+          960: { items: 2, margin: 150 },
+          1050: { items: 2, margin: 210 },
+          1200: { items: 2, margin: -100 },
+        }}
       >
-        <Card title="Ryan Ronalds" revRotate/>
+        <Card title="Ryan Ronalds" revRotate />
         <Card title="Ryan Ronalds" />
-        <Card title="Ryan Ronalds" revRotate/>
+        <Card title="Ryan Ronalds" revRotate />
         <Card title="Ryan Ronalds" />
       </OwlCarousel>
     </>
