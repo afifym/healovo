e1dba63522f6e864678f9cbadc28c4da884a49ad
afifym <mohamed.afify323@gmail.com>
2021-04-16 22:20:37 +0200

    installed mui icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PatientDashboard from './pages/PatientDashboard';
-import AdminDashboard from './pages/AdminDashboard';
+import Admin from './pages/Admin';
 import DoctorDashboard from './pages/DoctorDashboard';
 import Search from './pages/Search';
 import DoctorProfile from './pages/DoctorProfile';
@@ -9,22 +9,20 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Book from './pages/Book';
 
-
-
 function App() {
   return (
     <Router>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={Signup} />
-          <Route path='/search' component={Search} />
-          <Route path='/doctors/:id' component={DoctorProfile} />
-          <Route path='/book/:id' component={Book} />
-          <Route path='/patient-dashboard' component={PatientDashboard} />
-          <Route path='/doctor-dashboard' component={DoctorDashboard} />
-          <Route path='/admin-dashboard' component={AdminDashboard} />
-        </Switch>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/search' component={Search} />
+        <Route path='/doctors/:id' component={DoctorProfile} />
+        <Route path='/book/:id' component={Book} />
+        <Route path='/patient-dashboard' component={PatientDashboard} />
+        <Route path='/doctor-dashboard' component={DoctorDashboard} />
+        <Route path='/admin-dashboard' component={Admin} />
+      </Switch>
     </Router>
   );
 }
--- src/pages/AdminDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AdminDashboard = () => {
-  return <h2>AdminDashboard</h2>;
-};
-
-export default AdminDashboard;
