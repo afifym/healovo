9fc1b18a5fe1e812b420a5c776a4e10b56369e0e
afifym <mohamed.afify323@gmail.com>
2021-04-17 00:01:15 +0200

    reverted to initial

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,16 +1,5 @@
-import React from 'react';
-import AdminPanel from '../components/AdminPanel/AdminPanel';
-import { Admin as ReactAdmin, Resource } from 'react-admin';
-import restProvider from 'ra-data-simple-rest';
-
 const Admin = () => {
-  return (
-    <ReactAdmin
-      dataProvider={restProvider('https://healovo-default-rtdb.firebaseio.com')}
-    >
-      <AdminPanel />
-    </ReactAdmin>
-  );
+  return <h2>Admin</h2>;
 };
 
 export default Admin;
