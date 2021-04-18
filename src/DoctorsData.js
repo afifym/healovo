5b95419d128ec29a9e82a0d95681a6c8a4608f5e
mohand abdalbari <mohand.mostafa9811@gmail.com>
2021-04-19 00:47:34 +0200

    add functionality to searchBy buttons

--- src/DoctorsData.js
+++ src/DoctorsData.js
@@ -1,29 +1,21 @@
 export const DoctorsData = [
   {
     id: 1,
+    avtarImg: "doctorAvtar.png",
+    name: { firstName: "mohand", lastName: "mostafa" },
     degree: "Masters in medicine",
     Specialty: "Respiratory Specialist",
     rate: 2,
-    location: "Luxor Egypt",
+    location: { Governorate: "Luxor", Country: "Egypt" },
     CommunicationMethods: { clinc: true, home: true, video: true },
     ReservationDates: [
       { day: "Mon", availablePeriod: "1:00 pm - 6:00 pm" },
       { day: "Tue", availablePeriod: "1:00 pm - 6:00 pm" },
       { day: "Wed", availablePeriod: "5:00 pm - 12:00 pm" },
     ],
-    price: "35",
-    doctorInfo: {
-      name: "mohand mostafa",
-      phone: "01288040837",
-      email: "mohand.mostafa9811@gmail.com",
-      password: "hamada",
-      gender: "male",
-      joinDate: "11-11-2011", //new Date().toISOString().slice(0, 10)
-      image: "doctorAvtar.png",
-    },
+    price: 35,
   },
   {
-    /*image*/
     id: 2,
     avtarImg: "doctorAvtar2.png",
     name: { firstName: "mohamed", lastName: "Fwazy" },
--- src/components/SearchPage/SearchBar.jsx
+++ src/components/SearchPage/SearchBar.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import CustomeSearchInput from "./CustomeSearchInput";
 import CustomeSelectInput from "./CustomeSelectInput";
 import { FaCity, FaUserMd } from "react-icons/fa";
+import { useState } from "react";
 
 const SearchBarWraper = styled.div`
     display:flex;
@@ -16,10 +17,10 @@ const SearchBarWraper = styled.div`
 `;
 
 const SearchTabBtn = styled(Button)`
-  background: #ffffff !important;
+  background: #ffffff;
   box-shadow: 0px 4px 4px rgba(156, 156, 156, 0.25) !important;
 
-  color: black !important;
+  color: black;
   font-family: Montserrat;
   font-style: normal !important;
   font-weight: 700 !important;
@@ -29,6 +30,12 @@ const SearchTabBtn = styled(Button)`
 
   font-style: normal;
   font-weight: bold;
+  &:hover {
+    background: lightgray;
+    box-shadow: 0px 4px 4px rgba(156, 156, 156, 0.25) !important;
+
+    color: white;
+  }
 `;
 const CustomeButtonGroup = styled(ButtonGroup)`
   border-radius: 100px !important;
@@ -37,8 +44,30 @@ const CustomeButtonGroup = styled(ButtonGroup)`
 `;
 
 const SearchBar = () => {
-  const HandleClick = (e) => {
-    console.log("event", e.target);
+  const [searchBy, setSearchBy] = useState("name");
+  const activeBtn = {
+    backgroundColor: "#2D50EF",
+    color: "white",
+  };
+  const showSearchInput = () => {
+    switch (searchBy) {
+      case "name":
+        return <CustomeSearchInput />;
+      case "specialty":
+        return (
+          <CustomeSelectInput
+            SelectIconName={FaCity}
+            selectData={["mohand", "mostafa", "abadlabary"]}
+          />
+        );
+      case "city":
+        return (
+          <CustomeSelectInput
+            SelectIconName={FaUserMd}
+            selectData={["mohand", "mostafa", "abadlabary"]}
+          />
+        );
+    }
   };
   return (
     <SearchBarWraper>
@@ -47,33 +76,28 @@ const SearchBar = () => {
         color="primary"
         aria-label="contained primary button group"
       >
-        <Button>click</Button>
-        <SearchTabBtn onClick={HandleClick} name="name">
+        <SearchTabBtn
+          onClick={() => setSearchBy("name")}
+          style={searchBy == "name" ? activeBtn : {}}
+          name="name"
+        >
           Name
         </SearchTabBtn>
-        <SearchTabBtn onClick={HandleClick} name="specialty">
+        <SearchTabBtn
+          style={searchBy == "specialty" ? activeBtn : {}}
+          onClick={() => setSearchBy("specialty")}
+        >
           Specialty
         </SearchTabBtn>
         <SearchTabBtn
-          disableFocusRipple={true}
-          disableElevation={true}
-          onClick={HandleClick}
+          style={searchBy == "city" ? activeBtn : {}}
+          onClick={() => setSearchBy("city")}
           name="City"
         >
           City
         </SearchTabBtn>
       </CustomeButtonGroup>
-
-      <CustomeSearchInput />
-
-      <CustomeSelectInput
-        SelectIconName={FaCity}
-        selectData={["mohand", "mostafa", "abadlabary"]}
-      />
-      <CustomeSelectInput
-        SelectIconName={FaUserMd}
-        selectData={["mohand", "mostafa", "abadlabary"]}
-      />
+      {showSearchInput()}
     </SearchBarWraper>
   );
 };
