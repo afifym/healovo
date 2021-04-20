558a892b568404a2ef6e5dbd1fc59cc42175d438
mohand abdalbari <mohand.mostafa9811@gmail.com>
2021-04-20 20:48:18 +0200

    upload file to github

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import PatientDashboard from './pages/PatientDashboard';
-import Admin from './pages/Admin';
-import DoctorDashboard from './pages/DoctorDashboard';
-import Search from './pages/Search';
-import DoctorProfile from './pages/DoctorProfile';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Book from './pages/Book';
-import NotFound from './pages/NotFound';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PatientDashboard from "./pages/PatientDashboard";
+import Admin from "./pages/Admin";
+import DoctorDashboard from "./pages/DoctorDashboard";
+import Search from "./pages/Search";
+import DoctorProfile from "./pages/DoctorProfile";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Book from "./pages/Book";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={Signup} />
-        <Route path='/search' component={Search} />
-        <Route path='/doctors/:id' component={DoctorProfile} />
-        <Route path='/book/:id' component={Book} />
-        <Route path='/patient-dashboard' component={PatientDashboard} />
-        <Route path='/doctor-dashboard' component={DoctorDashboard} />
-        <Route path='/admin' component={Admin} />
-        <Route path='*' component={NotFound} />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/search" component={Search} />
+          <Route path="/doctors/:id" component={DoctorProfile} />
+          <Route path="/book/:id" component={Book} />
+          <Route path="/patient-dashboard" component={PatientDashboard} />
+          <Route path="/doctor-dashboard" component={DoctorDashboard} />
+          <Route path="/admin" component={Admin} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import MetaDecorator from '../components/shared/MetaDecorator/MetaDecorator';
+import React from "react";
+import MetaDecorator from "../components/shared/MetaDecorator/MetaDecorator";
+import SearchCard from "./../components/SearchCard";
 
 const Home = () => {
   return (
     <div>
       <MetaDecorator
-        title='Healovo | Home'
-        description='A medical booking website that connects doctors with patients'
+        title="Healovo | Home"
+        description="A medical booking website that connects doctors with patients"
       />
-      <h2>Home</h2>
+      <SearchCard />
     </div>
   );
 };
